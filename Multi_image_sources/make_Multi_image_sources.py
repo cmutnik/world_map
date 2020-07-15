21d4d6ff631c91c2b58d782d8e438c4c79b99365
Corey Mutnik <cmutnik@hawaii.edu>
2020-07-15 14:14:02 -0400

    map updated to include images rotated properly

--- Multi_image_sources/make_Multi_image_sources.py
+++ Multi_image_sources/make_Multi_image_sources.py
@@ -63,30 +63,47 @@ def makeThumbnails(path_to_original_images, path_for_thumnails, imgsize, extensi
     if not os.path.exists(path_for_thumnails):
         os.makedirs(path_for_thumnails)
 
-    # check correct number of images were added to map
-    #counter = 0
-
     for photos in os.listdir(path_to_original_images):
-        #if photos.endswith(".jpg") or photos.endswith(".JPG") or photos.endswith(".png") or photos.endswith(".PNG"):
         # only run loop over files that have correct extensions
         if photos.endswith(extensions):
             outFilename = path_for_thumnails + photos
             
             # check if thumbnail already exists
             if os.path.isfile(outFilename):
-                print('Thumbnail already exists for: ', photos)
+                print('Thumbnail already exists for: %s ' % photos)
             else:
-                # open and resize images     
+                # open image
                 img = Image.open(path_to_original_images + photos)
+
+                #####
+                # Make sure thumbnails are rotated correctly
+                #####
+                # use try to only apply code to images with exif data
+                try:
+                    # get orientation value from exif data
+                    image_exif = img._getexif()
+                    image_orientation = image_exif[274]
+
+                    # rotate images based on orientation value
+                    if image_orientation == 3:
+                        img = img.rotate(180)
+                    if image_orientation == 6:
+                        img = img.rotate(-90)
+                    if image_orientation == 8:
+                        img = img.rotate(90)  
+                except:
+                    pass
+
+                # once rotated, make a thumbnail of each image
                 img.thumbnail([imgsize, imgsize],Image.ANTIALIAS)
 
-                # save ouput file
+                # save ouput image
                 img.save(outFilename)
                 
-                print('thumbnail made for: ', photos)
+                print('thumbnail made for: %s' % photos)
         else:
             #print(photos + ' doesnt have the correct extension, a thumbnail was not made')
-            print('thumbnail not made for: ', photos)
+            print('thumbnail not made for: %s' % photos)
 
 #def makePhotoMap(mapname='./Beth_Jeff_Adventures_thumbnails.html'):
 def makePhotoMap(mapname, imgsize, path_for_thumnails):
@@ -95,18 +112,6 @@ def makePhotoMap(mapname, imgsize, path_for_thumnails):
     '''
     # initialize map
     m = folium.Map([42.3730,-73.3677], zoom_start=3, tiles='Stamen Terrain')
-    '''
-    # folium tiles determine the type of map to use
-    m = folium.Map([42.3730,-73.3677], zoom_start=3, tiles='Stamen Terrain')
-    # OTHER TILES OPTIONS:
-        tiles='cartodbpositron',
-        ”OpenStreetMap”
-        ”Stamen Terrain”, “Stamen Toner”, “Stamen Watercolor”
-        ”CartoDB positron”, “CartoDB dark_matter”
-        ”Mapbox Bright”, “Mapbox Control Room” (Limited zoom)
-        ”Cloudmade” (Must pass API key)
-        ”Mapbox” (Must pass API key)
-    '''
     
     def addimagestomap(df, pincolor):
         # append thumbnail directory to image names
@@ -115,28 +120,13 @@ def makePhotoMap(mapname, imgsize, path_for_thumnails):
         imgpaths=df[df.columns[0]].to_list()
         
         # batch coordinates for each image
-        #imgcoords=[[df['lat'][i], df['long'][i]] for i in range(len(df))]
         imgcoords=[[df[df.columns[1]][i], df[df.columns[2]][i]] for i in range(len(df))]
-
-        # make list of image descriptions
-        #describeimgs=df[df.columns[3]].to_list()
-
-        #m = folium.Map(imgcoords[0], zoom_start=8)
 
         testNOloop = [folium.Html('<img src='+imgpaths[i]+'>', script=True) for i in range(len(imgpaths))]   
         
         #####
         # add marker for each popup
         #####
-        # add marker (no popup)
-        #[folium.Marker(imgcoords[i]).add_to(m) for i in range(len(imgcoords))] 
-        # use a regular polygon as a marker, with popup
-        #[folium.RegularPolygonMarker(location=imgcoords[j], popup=popup1[j],).add_to(m) for j in range(len(imgcoords))]
-        # use standard blue marker, with popup
-        #popup1 = [folium.Popup(testNOloop[i], max_width=imgsize) for i in range(len(testNOloop))]
-        #[folium.Marker(location=imgcoords[j], popup=popup1[j],).add_to(m) for j in range(len(imgcoords))]
-        #
-        #[folium.RegularPolygonMarker(
         [folium.Marker(
         location=imgcoords[j],
         popup=folium.Popup(testNOloop[j], max_width=imgsize),
@@ -144,10 +134,7 @@ def makePhotoMap(mapname, imgsize, path_for_thumnails):
         icon=folium.Icon(color=pincolor, icon='picture')
         ).add_to(m)  for j in range(len(imgcoords))]
 
-        #counter += [1 for _ in range(len(imgcoords))]
-        #counter += len(imgcoords)
         counter = len(imgcoords)
-
         return print(counter, ' ' + pincolor + ' pins added')
     
 
@@ -159,8 +146,7 @@ def makePhotoMap(mapname, imgsize, path_for_thumnails):
     df_dukes = pd.read_csv('./photos/dukesPics.csv')
     # add dukes images to map
     addimagestomap(df=df_dukes, pincolor='red')
-
-    #counter = 0 # reset counter
+    
     # do for all images in takos image file
     df_tako = pd.read_csv('./photos/TakosPics.csv')
     # add takos images to map
@@ -168,7 +154,7 @@ def makePhotoMap(mapname, imgsize, path_for_thumnails):
 
     # save completed map
     m.save(mapname)
-
+    
     return print(mapname + ' was made')
 
 
